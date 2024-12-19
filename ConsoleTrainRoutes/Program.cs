a41d7056585fe743766dd33654407c82158e1bc4
Marjeck Palacpac <marjeck@advabilities.com>
2024-12-19 08:56:48 +0800

    Create interface and service for TrainRoutes. Create CalculateRouteDistance method to calculate the route distance. Output the results in the console

--- ConsoleTrainRoutes/Program.cs
+++ ConsoleTrainRoutes/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleTrainRoutes.Services;
+
+try
+{
+    ITrainRoutesService service = new TrainRoutesService();
+
+    Console.WriteLine($"Test #1: {service.CalculateRouteDistance(new List<string> { "A", "B", "C" })}");
+    Console.WriteLine($"Test #2: {service.CalculateRouteDistance(new List<string> { "A", "D" })}");
+    Console.WriteLine($"Test #3: {service.CalculateRouteDistance(new List<string> { "A", "D", "C" })}");
+    Console.WriteLine($"Test #4: {service.CalculateRouteDistance(new List<string> { "A", "E", "B", "C", "D" })}");
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine($"Error: {ex.Message}");
+}
---
+++ ConsoleTrainRoutes/Services/TrainRoutesService.cs
@@ -0,0 +1,28 @@
+﻿using ConsoleTrainRoutes.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTrainRoutes.Services
+{
+    public  class TrainRoutesService : ITrainRoutesService
+    {
+        public int CalculateRouteDistance(List<string> towns)
+        {
+            int distance = 0;
+
+            for (int i = 0; i < towns.Count - 1; i++)
+            {
+                var route = Route.Routes.FirstOrDefault(r => r.From == towns[i] && r.To == towns[i + 1]);
+
+                if (route == null) return -1; // Route does not exist
+
+                distance += route.Distance;
+            }
+
+            return distance;
+        }
+    }
+}
